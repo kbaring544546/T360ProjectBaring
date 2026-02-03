d45baa87b0dee1e56a76163ecacf6dc3647c2b5a
kbaring544546 <k.baring.544546@umindanao.edu.ph>
2026-02-03 21:26:03 +0800

    Delete View directory

--- View/__init__.py
+++
@@ -1,7 +0,0 @@
-from .userProductManageView import AdminView
-from .loginView import LoginView
-from .posView import mainPosView, cartView
-
-__all__ = ['LoginView', 'AdminView', 'mainPosView', 'cartView']
-
-
--- View/userProductEdit/__init__.py
+++
@@ -1,5 +0,0 @@
-from .userManagementSection import UserManagementSection
-from .productManagementSection import ProductManagementSection
-
-__all__ = ['UserManagementSection', 'ProductManagementSection']
-
