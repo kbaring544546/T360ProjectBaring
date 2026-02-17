5628d50dee35ffdd3a09e201aae8efd896d0a29e
kervy747 <136481876+kervy747@users.noreply.github.com>
2026-02-17 19:33:28 +0800

    Delete Controller directory

--- Controller/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Controller package for POS System
-Separates controller logic into organized modules
-"""
-
-from .main_controller import POSController
-from .overview_controller import OverviewController
-
-__all__ = ['POSController', 'OverviewController']
